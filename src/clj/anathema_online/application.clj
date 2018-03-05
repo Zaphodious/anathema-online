5660f7033ad77c68b4050cd963de9e35f42c4334
Alex Chythlook <achythlook@gmail.com>
2018-03-04 21:33:52 -0900

    Revert a bunch of stuff to make things work. Also put favicon in, I guess?

--- src/clj/anathema_online/application.clj
+++ src/clj/anathema_online/application.clj
@@ -6,21 +6,20 @@
             [system.components.middleware :refer [new-middleware]]
             [system.components.jetty :refer [new-web-server]]
             [anathema-online.config :refer [config]]
-            [anathema-online.routes :refer [home-routes]]
-            [anathema-online.data :as d]))
+            [anathema-online.routes :refer [home-routes]]))
 
 (defn app-system [config]
   (component/system-map
-   :routes     (new-endpoint home-routes)
-   :middleware (new-middleware {:middleware (:middleware config)})
-   :handler    (-> (new-handler)
-                   (component/using [:routes :middleware]))
-   :http       (-> (new-web-server (:http-port config))
-                   (component/using [:handler]))))
+    :routes     (new-endpoint home-routes)
+    :middleware (new-middleware {:middleware (:middleware config)})
+    :handler    (-> (new-handler)
+                    (component/using [:routes :middleware]))
+    :http       (-> (new-web-server (:http-port config))
+                    (component/using [:handler]))))
 
 (defn -main [& _]
   (let [config (config)]
-    (d/restore-dev-db!)
     (-> config
         app-system
-        component/start)))
+        component/start)
+    (println "Started anathema-online on" (str "http://localhost:" (:http-port config)))))
--- src/clj/anathema_online/routes.clj
+++ src/clj/anathema_online/routes.clj
@@ -1,21 +1,15 @@
 (ns anathema-online.routes
   (:require [clojure.java.io :as io]
             [compojure.core :refer [ANY GET PUT POST DELETE routes]]
-            [compojure.route :as route :refer [resources]]
+            [compojure.route :refer [resources]]
             [ring.util.response :refer [response]]))
 
 (defn home-routes [endpoint]
   (routes
-    (route/not-found
-      (-> "public/index.html"
-          io/resource
-          io/input-stream
-          response
-          (assoc :headers ["Content-Type" "text/html; charset=utf-8"])))
     (GET "/" _
       (-> "public/index.html"
           io/resource
           io/input-stream
           response
-         (assoc :headers {"Content-Type" "text/html; charset=utf-8"})))
+          (assoc :headers {"Content-Type" "text/html; charset=utf-8"})))
     (resources "/")))
