75915242e241a9e056ce37b223c9b05bdaf99441
Alex Chythlook <achythlook@gmail.com>
2018-03-04 20:33:16 -0900

    Attempt to make heroku actually work... again.

--- src/clj/anathema_online/routes.clj
+++ src/clj/anathema_online/routes.clj
@@ -1,15 +1,19 @@
 (ns anathema-online.routes
   (:require [clojure.java.io :as io]
             [compojure.core :refer [ANY GET PUT POST DELETE routes]]
-            [compojure.route :refer [resources]]
+            [compojure.route :as route :refer [resources]]
             [ring.util.response :refer [response]]))
 
 (defn home-routes [endpoint]
   (routes
-   (GET "/" _
-     (-> "public/index.html"
-         io/resource
-         io/input-stream
-         response
+    (route/not-found
+      (-> "<body>Hello World</body>"
+          response
+          (assoc :headers ["Content-Type" "text/html; charset=utf-8"])))
+    (GET "/" _
+      (-> "public/index.html"
+          io/resource
+          io/input-stream
+          response
          (assoc :headers {"Content-Type" "text/html; charset=utf-8"})))
-   (resources "/")))
+    (resources "/")))
