35e5935f75734b7a4fc88b7e682931320f4a1410
Alex Chythlook <achythlook@gmail.com>
2018-03-12 00:24:39 -0800

    Add debugging bit for api, to make sure its actually Heroku that's busted.

--- src/clj/anathema_online/routes.clj
+++ src/clj/anathema_online/routes.clj
@@ -1,12 +1,15 @@
 (ns anathema-online.routes
   (:require [clojure.java.io :as io]
-            [compojure.core :refer [ANY GET PUT POST DELETE routes]]
+            [compojure.core :refer [ANY GET PUT POST DELETE routes context]]
             [compojure.route :refer [resources]]
             [anathema-online.disk :as adisk]
             [ring.util.response :as resp :refer [response]]))
 
 (defn home-routes [{:keys [disk] :as endpoint}]
   (routes
+    (context "/data" []
+      (GET "/" [] (resp/content-type (response "<h1>Thing</h1>") "text/html")))
+
     (GET "/data/:category/:key.:filetype" [category key filetype]
       (-> (adisk/read-object
             disk
