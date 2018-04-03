96be1ef25526272d2c878e7b2361d771a61b3b01
Alex Chythlook <achythlook@gmail.com>
2018-04-02 19:28:21 -0800

    Point index to the correct place, and use hireling to register the worker.

--- src/cljs/anathema_online/components/service_worker.cljs
+++ src/cljs/anathema_online/components/service_worker.cljs
@@ -1,26 +1,11 @@
 (ns anathema-online.components.service-worker
-  (:require [com.stuartsierra.component :as component]))
-
-(def worker-path (if js/goog.DEBUG
-                   "/jshelper/bootstrap_worker.js"
-                   "/js/compiled/service_worker.js"))
-
-(defn is-service-worker-supported?
-  []
-  (exists? js/navigator.serviceWorker))
-
-(defn register-service-worker
-  [path-to-sw]
-  (if (is-service-worker-supported?)
-    (-> js/navigator
-        .-serviceWorker
-        (.register path-to-sw)
-        (.then #(js/console.log (str "Service Worker Registered[" path-to-sw "]"))))))
+  (:require [com.stuartsierra.component :as component]
+            [hireling.core :as hireling]))
 
 (defrecord Service-Worker-Component []
   component/Lifecycle
   (start [this]
-    (register-service-worker "/js/compiled/service_worker.js")
+    (hireling/register-service-worker "/service_worker.js")
     this)
   (stop [this] this))
 
